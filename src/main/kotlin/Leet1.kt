c454461954340f5c04b7863207e858f9e1c33743
Ondrej Zizka <zizka@seznam.cz>
2022-01-27 03:09:43 +0100

    Leet1

---
+++ src/main/kotlin/Leet1.kt
@@ -0,0 +1,19 @@
+class Leet1 {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val counts = nums.asList().groupingBy { it }.eachCount()
+
+        for (cur in nums) {
+            val counterPart = target - cur
+            val hit = counts[counterPart]
+            if (hit == null) continue
+            if (cur == counterPart) {
+                if (hit == 1) continue
+            }
+            val asList = nums.toList()
+            val counterpartIndex = asList.lastIndexOf(counterPart)
+            return intArrayOf(asList.indexOf(cur), counterpartIndex)
+        }
+        return intArrayOf(-1, -1)
+    }
+}
+
