8c49e0758abc67df881d9ba285fad3064d305d3a
Ondrej Zizka <zizka@seznam.cz>
2022-01-29 20:43:02 +0100

    Leet94 success

---
+++ src/main/kotlin/Leet94_inOrderTree.kt
@@ -0,0 +1,16 @@
+class Leet94_inOrderTree {
+
+    val list = mutableListOf<Int>()
+
+    fun inorderTraversal(root: TreeNode?): List<Int> {
+        iot(root)
+        return list
+    }
+
+    fun iot(root: TreeNode?) {
+        if (root == null) return
+        iot(root.left)
+        list.add(root.`val`)
+        iot(root.right)
+    }
+}
