661577a88769ff28609e9b5ee445ac27474156e3
Ondrej Zizka <zizka@seznam.cz>
2022-01-29 16:14:17 +0100

    Leet 5 - SUCCESS

--- src/main/kotlin/Leet5.kt
+++ src/main/kotlin/Leet5.kt
@@ -2,49 +2,70 @@ import java.util.*
 
 class Leet5 {
 
-    class Context {
-        val palindromes = mutableMapOf<IntRange, Boolean>()
-        val lettersPos = mutableMapOf<Char, TreeSet<Int>>()
-    }
-
-
     fun longestPalindrome(s: String): String {
 
-        val ctx = Context()
-        s.forEachIndexed { i, c ->
-            ctx.lettersPos.computeIfAbsent(c) { TreeSet() }.add(i)
-        }
-
-        var longest = s
-        var maxLen = 1
+        val maxIndex = s.length-1
+        var longest = ""
+        var maxLen = 0
         var pos = 0
 
         for (c in s) {
-            for (otherPos in ctx.lettersPos.get(c)!!.tailSet(pos+1)) {
-                if (isPalindrome(s, pos, otherPos, ctx) == true) {
-                    longest = s.substring(pos, otherPos)
-                    maxLen = otherPos - pos
+            // Odd
+            var offset = 0
+            while (offset <= pos && offset <= maxIndex - pos) {
+                if (s[pos - offset] != s[pos + offset])
+                    break;
+
+                val curPalLen = 1 + 2 * offset
+                if (curPalLen > maxLen) {
+                    maxLen = curPalLen
+                    longest = s.substring(pos - offset, pos + offset +1)
+                }
+                offset++
+            }
+
+            // Even
+            if (pos != maxIndex && s[pos] == s[pos+1]) {
+                offset = 0
+                while (offset <= pos && 1 + offset <= maxIndex - pos) {
+                    if (s[pos - offset] != s[pos +1 + offset])
+                        break;
+
+                    val curPalLen = 2 + 2* offset
+                    if (curPalLen > maxLen) {
+                        maxLen = curPalLen
+                        longest = s.substring(pos - offset, pos +1 + offset +1)
+                    }
+                    offset++
                 }
             }
+
             pos++
         }
 
+        return longest
     }
 
-    private fun isPalindrome(s: String, pos: Int, otherPos: Int, ctx: Context, ): Boolean {
-        // Assumes the equality of the edges chars was already checked.
+}
 
-        if (pos == otherPos) return true
-        if (pos == otherPos - 3) return true
-
-        if (true == ctx.palindromes.get(pos..otherPos)) return true
-
-        var offset = 1
-        while (offset < (otherPos - pos) / 2) {
-            if (s[pos+offset] != s[otherPos-offset])
-                TODO()
-        }
-
+fun main() {
+    for (i in listOf(
+//        "" to "",
+        "a" to "a",
+//        "aa" to "aa",
+//        "aab" to "aa",
+//        "abb" to "bb",
+//        "aabb" to "aa",
+//        "aacbb" to "aa",
+//        "aaccbb" to "aa",
+//        "aacccbb" to "ccc",
+//        "aaaaa1" to "aaaaa",
+//        "aa1aa" to "aa1aa",
+    )) {
+        println("Trying ${i.first}")
+        val res = Leet5().longestPalindrome(i.first)
+        print("${i.first} => $res")
+        if (res != i.second) print("; EXPECTED ${i.second}") else print("  ...OK")
+        println()
     }
-
 }
