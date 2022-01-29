38f4219245142e3b3471dfb83ccb928167126344
Ondrej Zizka <zizka@seznam.cz>
2022-01-29 17:15:42 +0100

    Leet75 SUCCESS - gnome sort

---
+++ src/main/kotlin/Leet75_inplaceSort.kt
@@ -0,0 +1,16 @@
+class Leet75_inplaceSort {
+
+    fun sortColors(nums: IntArray): Unit {
+        var pos = 0
+        while (pos < nums.size) {
+            if (pos == 0 || nums[pos] >= nums[pos - 1])
+                pos++
+            else {
+                val tmp = nums[pos-1]
+                nums[pos-1] = nums[pos]
+                nums[pos] = tmp
+                pos--
+            }
+        }
+    }
+}
