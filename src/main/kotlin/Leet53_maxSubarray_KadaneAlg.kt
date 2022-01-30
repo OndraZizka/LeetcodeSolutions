0ae539c5c376d56a3daf23d76e399b57f7c084f1
Ondrej Zizka <zizka@seznam.cz>
2022-01-30 20:52:55 +0100

    Leet53 success

---
+++ src/main/kotlin/Leet53_maxSubarray_KadaneAlg.kt
@@ -0,0 +1,18 @@
+class Leet53_maxSubarray_KadaneAlg {
+
+    fun maxSubArray(nums: IntArray): Int {
+
+        var curLargerSum = 0
+        var maxSum = java.lang.Integer.MIN_VALUE
+
+        for (i in nums) {
+            curLargerSum += i
+            if (i > curLargerSum)
+                curLargerSum = i
+            if (curLargerSum > maxSum)
+                maxSum = curLargerSum
+        }
+        return maxSum
+    }
+
+}
