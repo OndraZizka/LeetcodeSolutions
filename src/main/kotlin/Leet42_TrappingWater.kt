248f710c53b3702dff685f19eff1deb008c4241e
Ondrej Zizka <zizka@seznam.cz>
2022-01-30 22:42:11 +0100

    Leet42 Trapping Water - second attempt - with 2 pointers solution. SUCCESS

--- src/main/kotlin/Leet42_TrappingWater.kt
+++ src/main/kotlin/Leet42_TrappingWater.kt
@@ -2,41 +2,39 @@ class Leet42_TrappingWater {
 
     fun trap(height: IntArray): Int {
 
-        val maxOnLeft  = IntArray(height.size)
-        val maxOnRight = IntArray(height.size)
-
-        // Left maxes
-        var curMax = 0
-        height.forEachIndexed { i, h ->
-            maxOnLeft[i] = curMax
-            if (h > curMax) {
-                curMax = h
-            }
-        }
-
-        curMax = 0
-        for (i in (height.size - 1)downTo 0) {
-            val h  = height[i]
-            maxOnRight[i] = curMax
-            if (h > curMax) {
-                curMax = h
-            }
-        }
-
+        var posL = 0
+        var posR = height.size - 1
+        var maxL = height[posL]
+        var maxR = height[posR]
         var sumWater = 0
-        height.forEachIndexed { i, h ->
-            val maxL = maxOnLeft[i]
-            val maxR = maxOnRight[i]
-            val waterHere = (if (maxL < maxR) maxL else maxR) - h
-            if (waterHere > 0)
-                sumWater += waterHere
-        }
 
+        while (posL != posR) {
+
+            val localPos: Int
+            val localHighestSpillingDam =
+                if (maxL < maxR) {
+                    val localDam = Math.min(maxL, maxR)
+                    localPos = ++posL
+                    maxL = Math.max(maxL, height[posL])
+                    localDam
+                } else {
+                    val localDam = Math.min(maxL, maxR)
+                    localPos = --posR
+                    maxR = Math.max(maxR, height[posR])
+                    localDam
+                }
+
+            val waterHere = localHighestSpillingDam - height[localPos]
+            if (waterHere > 0) {
+                sumWater += waterHere
+            }
+            println ("[$localPos] h: ${height[localPos]}, localDam: $localHighestSpillingDam; Adding $waterHere")
+        }
         return sumWater
     }
 }
 
 fun main() {
-    val trap = Leet42_TrappingWater().trap(intArrayOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1))
+    val trap = Leet42_TrappingWater().trap(intArrayOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)) // 6
     println("Trapped: $trap")
 }
