f8f2da522993681665956d5ef150161946441356
Ondrej Zizka <zizka@seznam.cz>
2022-01-30 22:09:28 +0100

    Leet42 Trapping Water - second attempt - normal IntArray. SUCCESS

--- src/main/kotlin/Leet42_TrappingWater.kt
+++ src/main/kotlin/Leet42_TrappingWater.kt
@@ -1,36 +1,35 @@
-import kotlin.math.min
-
 class Leet42_TrappingWater {
 
     fun trap(height: IntArray): Int {
 
-        val maxesOnLeft = mutableListOf<Pair<Int, Int>>()  // Height, position
-        val maxesOnRight = mutableListOf<Pair<Int, Int>>()  // Height, position
+        val maxOnLeft  = IntArray(height.size)
+        val maxOnRight = IntArray(height.size)
 
         // Left maxes
         var curMax = 0
         height.forEachIndexed { i, h ->
+            maxOnLeft[i] = curMax
             if (h > curMax) {
                 curMax = h
-                maxesOnLeft.add(Pair(h, i))
             }
         }
 
         curMax = 0
         for (i in (height.size - 1)downTo 0) {
             val h  = height[i]
+            maxOnRight[i] = curMax
             if (h > curMax) {
                 curMax = h
-                maxesOnRight.add(Pair(h, i))
             }
         }
 
         var sumWater = 0
         height.forEachIndexed { i, h ->
-            val maxL = maxesOnLeft.asSequence().filter { it.second < i }.lastOrNull()?.first ?: 0
-            val maxR = maxesOnRight.asSequence().filter { it.second > i }.lastOrNull()?.first ?: 0
-            val waterHere = Math.min(maxL, maxR) - h
-            sumWater += Math.max(0, waterHere)
+            val maxL = maxOnLeft[i]
+            val maxR = maxOnRight[i]
+            val waterHere = (if (maxL < maxR) maxL else maxR) - h
+            if (waterHere > 0)
+                sumWater += waterHere
         }
 
         return sumWater
@@ -38,5 +37,6 @@ class Leet42_TrappingWater {
 }
 
 fun main() {
-    Leet42_TrappingWater().trap(intArrayOf(0,1,0,2,1,0,1,3,2,1,2,1))
+    val trap = Leet42_TrappingWater().trap(intArrayOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1))
+    println("Trapped: $trap")
 }
