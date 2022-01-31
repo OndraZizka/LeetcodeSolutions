02a919c84c80d3fbd63c852f62c64174fb88bb31
Ondrej Zizka <zizka@seznam.cz>
2022-01-31 01:03:16 +0100

    Leet1189 Maximum no of "balloon"s (stupid one)

---
+++ src/main/kotlin/Leet1189_baloons_easy.kt
@@ -0,0 +1,22 @@
+public class Leet1189_baloons_easy
+{
+    fun maxNumberOfBalloons(text: String): Int {
+    var b = 0
+    var a = 0
+    var l = 0
+    var o = 0
+    var n = 0
+
+    for (c in text) {
+        when (c) {
+            'b' -> b+=2
+            'a' -> a+=2
+            'l' -> l++
+            'o' -> o++
+            'n' -> n+=2
+        }
+    }
+    //println("" + listOf(b,a,l,o,n))
+    return Math.min(Math.min(Math.min(Math.min(b,a),l),o),n) / 2
+}
+}
