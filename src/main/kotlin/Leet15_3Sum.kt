1ffc6c9cdc5619af884d1675d5aaf68fa545825c
Ondrej Zizka <zizka@seznam.cz>
2022-02-01 13:44:57 +0100

    Leet15: 3Sum - SUCCESS, WITH counting of the duplicates in the array. Still not using sorting,

--- src/main/kotlin/Leet15_3Sum.kt
+++ src/main/kotlin/Leet15_3Sum.kt
@@ -4,49 +4,43 @@ class Leet15_3Sum {
     data class Triplet(val items: List<Int>)
 
     fun threeSum(nums: IntArray): List<List<Int>> {
-        val res = mutableSetOf<Triplet>()
+        val resSet = mutableSetOf<Triplet>()
+        val freq = nums.asList().groupingBy { it }.eachCount()
 
         fun findCouplesThatSumToTarget(target: Int): List<IntArray> {
 
             println("Targetting $target...")
             val res = mutableListOf<IntArray>()
-            var targetSkippedOnce = false
-            var sameSkippedOnce = false
-            val filtered = nums
-                .filter {
-                    // Skip duplicate numbers
-                    if (-target == it && !targetSkippedOnce ) {
-                        targetSkippedOnce = true; false
-                    }
-                    else if (it == target - it && !sameSkippedOnce) {
-                        sameSkippedOnce = true; false
-                    }
-                    //    Adding: [ 2, -4]  (-2 - 2)  --- to tam byt nema
-                    else true
-                }
-            val complements = filtered.map { target - it }.toSet()
 
-            for (b in nums) {
-                //if (b == -target) continue
-                //if (target - b == -target) continue
-                if (complements.contains(b)) {
-                    println("   Adding: [${b.toString().padStart(2)}, ${target-b}]  ($target - $b)")
-                    res.add(intArrayOf(b, target - b))
-                }
+            @Suppress("NAME_SHADOWING")
+            val freq = HashMap<Int, Int>(freq)
+            freq.compute(-target) { key, count -> count?.minus(1) ?: 0 }
+
+            for (entry in freq) {
+                val b = entry.key
+                val complement = target - b
+                var complCount = freq.get(complement) ?: 0
+                if (b == complement) complCount--
+
+                println("   Considering: [${b.toString().padStart(2)} (${entry.value}x), ${complement} (${complCount}x)]  ($target - $b)")
+                if (entry.value != 0 && complCount > 0)
+                    res.add(intArrayOf(b, complement))
             }
+            println("    For $target --> added ${res.map { it.joinToString("+") }  }")
             return res
         }
 
         for (a in nums) {
             // Must find b + c = -a
             val pairs: List<IntArray> = findCouplesThatSumToTarget(-a)
-            res.addAll(pairs.map { Triplet(listOf(a, it[0], it[1])) })
+
+            resSet.addAll(pairs.map { Triplet(listOf(a, it[0], it[1]).sorted()) })
         }
 
         val res2 = mutableListOf<List<Int>>()
-        res2.addAll(res.map { it.items } )
+        res2.addAll(resSet.map { it.items } )
 
-        return res.asSequence().map { it -> it.items }.toList()
+        return resSet.asSequence().map { it -> it.items }.toList()
     }
 
 }
