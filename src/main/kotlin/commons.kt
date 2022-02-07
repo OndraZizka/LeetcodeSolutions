3a6a8fcbd8ebfcc6df0a77305871234be791a4c0
Ondrej Zizka <zizka@seznam.cz>
2022-02-07 01:43:05 +0100

    Commons - parsing integer pairs in brackets to Array<IntArray>

--- src/main/kotlin/commons.kt
+++ src/main/kotlin/commons.kt
@@ -1,3 +1,13 @@
 
 
 
+fun parsePairs(s: String): Array<IntArray> {
+
+    // E.g. [0,0],[2,2],[3,10],[5,2],[7,0]
+    return s.removeSuffix("]")
+        .splitToSequence("],[")
+        .map { it.removePrefix("[") }
+        .map { it.split(",").map { Integer.parseInt(it) }.toIntArray() }
+        .toList()
+        .toTypedArray()
+}
