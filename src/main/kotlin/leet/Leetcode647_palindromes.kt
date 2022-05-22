16013d4f7eb7416031b080b0a14a7e2995cdab30
Ondrej Zizka <zizka@seznam.cz>
2022-05-22 23:17:06 +0200

    Add Leetcode 647 - palindromes - counts also duplicates if on other position, but slow

--- src/main/kotlin/leet/Leetcode647_palindromes.kt
+++ src/main/kotlin/leet/Leetcode647_palindromes.kt
@@ -5,13 +5,13 @@ class Leetcode647_palindromes {
 
     fun countSubstrings(s: String): Int {
 
-        val pals = mutableSetOf<String>()
+        val pals = mutableSetOf<Pair<Int,Int>>()
 
         fun isPalindrome(l: Int, r: Int): Int {
             val substring = s.substring(l, r)
             if (substring == substring.reversed()) {
-                if (pals.add(substring)) {
-                    println("PAL: $substring")
+                if (pals.add(l to r)) {
+                    println("PAL: $l to $r - $substring")
                     return 1
                 }
             }
@@ -19,7 +19,8 @@ class Leetcode647_palindromes {
         }
 
         fun dfs(l: Int, r: Int): Int {
-            println("DOING $s, $l, $r ;  $r-$l > 1 ?   ${r-l > 1}")
+            if (pals.contains(l to r)) return 0
+            //println("DOING $s, $l, $r ;  $r-$l > 1 ?   ${r-l > 1}")
             var palCount = isPalindrome(l, r)
             if (r-l > 1) {
                 palCount += dfs(l + 1, r) + dfs(l, r - 1)
@@ -34,5 +35,6 @@ class Leetcode647_palindromes {
 
 fun main() {
     //println (Leetcode647_palindromes().countSubstrings("abc"))
-    println (Leetcode647_palindromes().countSubstrings("aaa"))
+    //println (Leetcode647_palindromes().countSubstrings("aaa"))
+    println (Leetcode647_palindromes().countSubstrings("xkjkqlajprjwefilxgpdpebieswu"))
 }
